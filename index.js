ddff46b99df05951025a45ea96ada83fc09187c5
jeliasalvaradoa <josealvarado30@hotmail.com>
2022-12-18 00:42:02 -0400

    Fucionando el archivo clase8.html del menu mobile en el index.html y los estilos en index.css

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const menuBurgerIcon = document.querySelector('.menu');
+const mobileMenu =document.querySelector('.mobile-menu');
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuBurgerIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() { 
     // quita o pone la clase inactive para que aparezca el desktop-menu
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
